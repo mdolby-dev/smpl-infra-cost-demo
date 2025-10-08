090dba11deb0c34686d4b52d26e69f8533d72567
mdolby-dev <dolbymatthew@gmail.com>
2025-10-08 07:32:48 -0700

    Update GCP resources for cost testing

--- gcp_resources_test.tf
+++ gcp_resources_test.tf
@@ -71,7 +71,7 @@ resource "google_pubsub_subscription" "example" {
 
   message_retention_duration = "604800s"  # 7 days
   retain_acked_messages     = true
-  ack_deadline_seconds      = 240
+  ack_deadline_seconds      = 2400
 }
 
 # Dataflow Job
@@ -86,7 +86,7 @@ resource "google_dataflow_job" "example" {
   }
 
   machine_type = "n1-standard-32"  # This affects the compute cost
-  max_workers  = 10                # This affects both compute and memory costs
+  max_workers  = 100                # This affects both compute and memory costs
 }
 
 # Storage Bucket for Dataflow
@@ -97,10 +97,176 @@ resource "google_storage_bucket" "dataflow_bucket" {
 
   lifecycle_rule {
     condition {
-      age = 30  # days
+      age = 120  # days
     }
     action {
       type = "Delete"
+    }
+  }
+}
+
+# Global storage bucket
+resource "google_storage_bucket" "global_bucket" {
+  name          = "my-global-bucket"
+  location      = "US"
+  force_destroy = true
+
+  uniform_bucket_level_access = true
+
+  lifecycle_rule {
+    condition {
+      age = 365
+    }
+    action {
+      type = "Delete"
+    }
+  }
+}
+
+# Pub/Sub DR storage bucket - us-central1
+resource "google_storage_bucket" "pubsub_dr_us_central1" {
+  name          = "pubsub-dr-us-central1"
+  location      = "us-central1"
+  force_destroy = true
+
+  uniform_bucket_level_access = true
+
+  lifecycle_rule {
+    condition {
+      age = 365
+    }
+    action {
+      type = "Delete"
+    }
+  }
+}
+
+# Pub/Sub DR storage bucket - us-west1
+resource "google_storage_bucket" "pubsub_dr_us_west1" {
+  name          = "pubsub-dr-us-west1"
+  location      = "us-west1"
+  force_destroy = true
+
+  uniform_bucket_level_access = true
+
+  lifecycle_rule {
+    condition {
+      age = 365
+    }
+    action {
+      type = "Delete"
+    }
+  }
+}
+
+# Firestore database (Native mode) in nam5
+resource "google_firestore_database" "nam5" {
+  name        = "(default)"
+  location_id = "nam5"
+  type        = "NATIVE"
+}
+
+# Storage bucket for Firestore backups (7-day retention)
+resource "google_storage_bucket" "firestore_backups_nam5" {
+  name          = "firestore-backups-nam5"
+  location      = "US"
+  force_destroy = true
+
+  uniform_bucket_level_access = true
+
+  lifecycle_rule {
+    condition {
+      age = 365
+    }
+    action {
+      type = "Delete"
+    }
+  }
+}
+
+# Pub/Sub Dead-Letter Queue (DLQ) topic
+resource "google_pubsub_topic" "example_dlq" {
+  name = "example-dlq-topic"
+}
+
+# Additional subscribers to the existing topic for cost testing
+resource "google_pubsub_subscription" "subscriber_a" {
+  name  = "example-sub-a"
+  topic = google_pubsub_topic.example.name
+
+  ack_deadline_seconds       = 600
+  message_retention_duration = "604800s"  # 7 days
+  retain_acked_messages      = false
+}
+
+resource "google_pubsub_subscription" "subscriber_b" {
+  name  = "example-sub-b"
+  topic = google_pubsub_topic.example.name
+
+  ack_deadline_seconds       = 600
+  message_retention_duration = "604800s"
+  retain_acked_messages      = false
+
+  dead_letter_policy {
+    dead_letter_topic      = google_pubsub_topic.example_dlq.id
+    max_delivery_attempts  = 5
+  }
+}
+
+resource "google_pubsub_subscription" "subscriber_c" {
+  name  = "example-sub-c"
+  topic = google_pubsub_topic.example.name
+
+  ack_deadline_seconds       = 600
+  message_retention_duration = "604800s"
+  retain_acked_messages      = false
+}
+
+# Managed GKE cluster - regional (us-central1)
+resource "google_container_cluster" "gke_us_central1" {
+  name     = "gke-us-central1"
+  location = "us-central1"
+
+  remove_default_node_pool = false
+  initial_node_count       = 12
+
+  node_config {
+    machine_type = "e2-standard-4"
+    preemptible  = false
+    disk_size_gb  = 100
+  }
+
+  # Enable basic add-ons useful for demos
+  addons_config {
+    horizontal_pod_autoscaling {
+      disabled = true
+    }
+    http_load_balancing {
+      disabled = false
+    }
+  }
+}
+
+# Managed GKE cluster - regional (us-west1)
+resource "google_container_cluster" "gke_us_west1" {
+  name     = "gke-us-west1"
+  location = "us-west1"
+
+  remove_default_node_pool = false
+  initial_node_count       = 12
+
+  node_config {
+    machine_type = "e2-standard-4"
+    preemptible  = false
+    disk_size_gb  = 1000
+  }
+
+  addons_config {
+    horizontal_pod_autoscaling {
+      disabled = true
+    }
+    http_load_balancing {
+      disabled = false
     }
   }
 }
